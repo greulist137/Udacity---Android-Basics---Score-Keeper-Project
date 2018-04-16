edefe84b96e08907c914b1ba4eab2762f3eb73e0
Steven Greulich <greulich.steven@gmail.com>
2018-04-15 18:19:06 -0700

    Commit - Completion of Player 1
    the purpose of this commit is that the player 1 functionality is now complete

--- app/src/main/java/com/example/greul/golfscorer/MainActivity.java
+++ app/src/main/java/com/example/greul/golfscorer/MainActivity.java
@@ -2,6 +2,10 @@ package com.example.greul.golfscorer;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.TextView;
+
+import org.w3c.dom.Text;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -10,4 +14,47 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
     }
+
+    int player1Score = 0;
+    int player1Hole = 1;
+    int player1HoleScore = 0;
+    int player2Score = 0;
+    int player2Hole = 1;
+    int player2HoleScore = 0;
+
+    public void increaseHolePlayer1(View view){
+        TextView player1HoleView = (TextView) findViewById(R.id.player_1_hole);
+        player1Hole += 1;
+        player1HoleView.setText(String.valueOf(player1Hole));
+        player1HoleScore = 0;
+
+    }
+
+    public void player1Stroke(View view){
+        TextView player1ScoreView = (TextView) findViewById(R.id.player_1_hole_score);
+        TextView player1HoleScoreView = (TextView) findViewById((R.id.player_1_score));
+        player1HoleScore += 1;
+        player1Score += 1;
+        player1HoleScoreView.setText(String.valueOf(player1HoleScore));
+        player1ScoreView.setText(String.valueOf(player1Score));
+    }
+
+    public void player1PenaltyOne(View view){
+        TextView player1ScoreView = (TextView) findViewById(R.id.player_1_hole_score);
+        TextView player1HoleScoreView = (TextView) findViewById((R.id.player_1_score));
+        player1HoleScore += 1;
+        player1Score += 1;
+        player1HoleScoreView.setText(String.valueOf(player1HoleScore));
+        player1ScoreView.setText(String.valueOf(player1Score));
+    }
+
+    public void player1PenaltyTwo(View view){
+        TextView player1ScoreView = (TextView) findViewById(R.id.player_1_hole_score);
+        TextView player1HoleScoreView = (TextView) findViewById((R.id.player_1_score));
+        player1HoleScore += 2;
+        player1Score += 2;
+        player1HoleScoreView.setText(String.valueOf(player1HoleScore));
+        player1ScoreView.setText(String.valueOf(player1Score));
+    }
+
 }
